a80017f1334706b7e77fd7bd2c39d60914fa686e
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 20:07:12 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,8 @@ end
 
 # Define your play method below
 def play(board)
-  until (turn_count = 9)
-    turn_count = 0
-
+  turn_count = 0
+  until (turn_count == 9)
     turn(board)
     turn_count += 1
   end
